68b67bdbf528987facc4894eec024477e3586e2f
SalahAmm <salahamm4year@gmail.com>
2025-08-04 15:10:25 +0100

    add Two features

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 //the game each Round
 
 const Gameboard = (() => {
-
   const board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
@@ -17,9 +16,59 @@ const Gameboard = (() => {
     return false;
   };
 
-    const resetBoard = () => {
+  const resetBoard = () => {
     board.fill("");
   };
 
-  return {getBoard , inputSign , resetBoard}
+  return { getBoard, inputSign, resetBoard };
+})();
+
+//Winning Condition :
+
+const WinningCondition = (() => {
+  const winningCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ]; 
+})();
+
+
+const PlayerData = (() => {
+
+
+const Player = (name , value) => {
+
+  return {name , value}
+};
+
+const board = Gameboard;
+
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player2' ,'O');
+
+let currentPlayer = player2;
+
+const switchTurn = () => {
+  currentPlayer = currentPlayer === player1 ? player2 : player1;
+};
+
+
+const getActivePlayer = () => currentPlayer;
+
+console.log(getActivePlayer(), board.getBoard());
+
+
+
+
+
+
+return {getActivePlayer, }
+
+
 })();
